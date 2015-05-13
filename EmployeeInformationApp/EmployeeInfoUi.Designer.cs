8b565e2539e3b7e682ab5c119451822101d5d79f
Rahman Tarik <tarikseu@gamil.com>
2015-05-13 19:50:30 +0600

    Without Layer

---
+++ EmployeeInformationApp/EmployeeInfoUi.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace EmployeeInformationApp
+{
+    partial class EmployeeInfoUi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveButton = new System.Windows.Forms.Button();
+            this.showAllButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.salaryTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(67, 236);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 0;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // showAllButton
+            // 
+            this.showAllButton.Location = new System.Drawing.Point(182, 235);
+            this.showAllButton.Name = "showAllButton";
+            this.showAllButton.Size = new System.Drawing.Size(75, 23);
+            this.showAllButton.TabIndex = 1;
+            this.showAllButton.Text = "Show All";
+            this.showAllButton.UseVisualStyleBackColor = true;
+            this.showAllButton.Click += new System.EventHandler(this.showAllButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(290, 235);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 2;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Salary";
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(139, 32);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(100, 20);
+            this.idTextBox.TabIndex = 6;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(139, 74);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(194, 20);
+            this.nameTextBox.TabIndex = 7;
+            // 
+            // salaryTextBox
+            // 
+            this.salaryTextBox.Location = new System.Drawing.Point(139, 110);
+            this.salaryTextBox.Name = "salaryTextBox";
+            this.salaryTextBox.Size = new System.Drawing.Size(151, 20);
+            this.salaryTextBox.TabIndex = 8;
+            // 
+            // EmployeeInfoUi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 356);
+            this.Controls.Add(this.salaryTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.idTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.showAllButton);
+            this.Controls.Add(this.saveButton);
+            this.Name = "EmployeeInfoUi";
+            this.Text = "Employee Information";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button showAllButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox salaryTextBox;
+    }
+}
+
---
+++ EmployeeInformationApp/EmployeeInfoUi.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmployeeInformationApp
+{
+    public partial class EmployeeInfoUi : Form
+    {
+        public EmployeeInfoUi()
+        {
+            InitializeComponent();
+        }
+
+        List<Employee> emplyeeList = new List<Employee>(); 
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (emplyeeList.Count <5)
+            {
+                Employee aEmployee = new Employee();
+                aEmployee.id = idTextBox.Text;
+                aEmployee.name = nameTextBox.Text;
+                aEmployee.salary = Convert.ToDouble(salaryTextBox.Text);
+                emplyeeList.Add(aEmployee);
+                ClearText();
+                MessageBox.Show("Saved");
+            }
+            else
+            {
+                MessageBox.Show(" Can't enter more then 5 employee ");
+            }
+            
+        }
+
+        private void showAllButton_Click(object sender, EventArgs e)
+        {
+            if (emplyeeList != null)
+            {
+
+
+                string employeeinfo = "";
+                foreach (Employee employee in emplyeeList)
+                {
+                    employeeinfo += employee.id + "\t" + employee.name + "\t" + employee.salary + "\n";
+                }
+
+                MessageBox.Show("ID\tName\tSalary\n" + employeeinfo);
+            }
+            else
+            {
+                MessageBox.Show("nothing to show");
+            }
+        }
+
+        private void ClearText()
+        {
+            idTextBox.Text = String.Empty;
+            nameTextBox.Text = String.Empty;
+            salaryTextBox.Text = String.Empty;
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            emplyeeList.Clear();
+            emplyeeList.Count.Equals(0);
+            MessageBox.Show("Cleared");
+            ClearText();
+        }
+    }
+}
